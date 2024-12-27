2b37354a6844b4eea6726104bfe2c6e313f96408
Binu Maka <binumaka9@gmail.com>
2024-12-27 14:57:27 +0545

    changes

--- lib/cubit/dashboard_cubit.dart
+++ lib/cubit/dashboard_cubit.dart
@@ -1,6 +1,8 @@
 import 'package:classassignment2/cubit/areaofcircle_cubit.dart';
+import 'package:classassignment2/cubit/arithmetic_cubit.dart';
 import 'package:classassignment2/cubit/simple_interest_cubit.dart';
 import 'package:classassignment2/view/areaofcircle_cubit_view.dart';
+import 'package:classassignment2/view/arithmetic_cubit_view.dart';
 import 'package:classassignment2/view/simple_interest_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -9,10 +11,12 @@ class DashboardCubit extends Cubit<void> {
   DashboardCubit(
     this._simpleinterestCubit,
     this._areaofcircleCubit,
+    this._arithmeticCubit,
   ) : super(null);
 
   final SimpleInterestCubit _simpleinterestCubit;
   final AreaOfCircleCubit _areaofcircleCubit;
+  final ArithmeticCubit _arithmeticCubit;
 
   void openSimpleInterestView(BuildContext context){
     Navigator.push(
@@ -31,6 +35,16 @@ class DashboardCubit extends Cubit<void> {
         builder: (_) => BlocProvider.value(
           value: _areaofcircleCubit,
           child: AreaOfCircleView()),
+      ),
+    );
+  }
+  void openArithmeticView(BuildContext context){
+    Navigator.push(
+      context, 
+      MaterialPageRoute(
+        builder: (_) => BlocProvider.value(
+          value: _arithmeticCubit,
+          child: ArithmeticCubitView()),
       ),
     );
   }
