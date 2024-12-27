43d3c8d2a9dad26ae26813cc5c0eb9fbc7272cd3
Binu Maka <binumaka9@gmail.com>
2024-12-27 14:59:36 +0545

    some changes

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,5 @@
 import 'package:classassignment2/cubit/areaofcircle_cubit.dart';
+import 'package:classassignment2/cubit/arithmetic_cubit.dart';
 import 'package:classassignment2/cubit/dashboard_cubit.dart';
 import 'package:classassignment2/cubit/simple_interest_cubit.dart';
 import 'package:classassignment2/view/dashboard_cubit_view.dart';
@@ -14,10 +15,12 @@ class App extends StatelessWidget {
       providers: [
         BlocProvider(create: (context) => SimpleInterestCubit()),
         BlocProvider(create: (context) => AreaOfCircleCubit()),
+        BlocProvider(create: (context) => ArithmeticCubit()),
         BlocProvider(
           create: (context) => DashboardCubit(
             context.read<SimpleInterestCubit>(),
             context.read<AreaOfCircleCubit>(),
+            context.read<ArithmeticCubit>(),
           ),
         ),
       ],
