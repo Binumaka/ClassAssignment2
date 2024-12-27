781becee7b4a872ce7b4dffea155d527e85e721a
Binu Maka <binumaka9@gmail.com>
2024-12-27 11:54:11 +0545

    app dart

---
+++ lib/app.dart
@@ -0,0 +1,31 @@
+import 'package:classassignment2/cubit/areaofcircle_cubit.dart';
+import 'package:classassignment2/cubit/dashboard_cubit.dart';
+import 'package:classassignment2/cubit/simple_interest_cubit.dart';
+import 'package:classassignment2/view/dashboard_cubit_view.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (context) => SimpleInterestCubit()),
+        BlocProvider(create: (context) => AreaOfCircleCubit()),
+        BlocProvider(
+          create: (context) => DashboardCubit(
+            context.read<SimpleInterestCubit>(),
+            context.read<AreaOfCircleCubit>(),
+          ),
+        ),
+      ],
+      child: const MaterialApp(
+        title: 'Flutter BLoC',
+        debugShowCheckedModeBanner: false,
+        home: DashboardView(),
+      ),
+    );
+  }
+}
